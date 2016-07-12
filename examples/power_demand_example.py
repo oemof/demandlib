09a64e3d21cab39920d63beca54a69fbfabe7276
uwe <uwe.krien@rl-institut.de>
2016-07-12 12:53:06 +0200

    adapt example

--- examples/power_demand_example.py
+++ examples/power_demand_example.py
@@ -2,12 +2,29 @@
 """Creating power demand profiles using bdew profiles.
 """
 
-import pandas as pd
+import datetime
 import demandlib.bdew as bdew
 import demandlib.particular_profiles as profiles
 from matplotlib import pyplot as plt
+import time
+start = time.time()
+year = 2010
 
-year = 2013
+# The following dictionary is create by "workalendar"
+# pip3 install workalendar
+# >>> from workalendar.europe import Germany
+# >>> cal = Germany()
+# >>> holidays = dict(cal.holidays(2010))
+holidays = {
+    datetime.date(2010, 5, 24): 'Whit Monday',
+    datetime.date(2010, 4, 5): 'Easter Monday',
+    datetime.date(2010, 5, 13): 'Ascension Thursday',
+    datetime.date(2010, 1, 1): 'New year',
+    datetime.date(2010, 10, 3): 'Day of German Unity',
+    datetime.date(2010, 12, 25): 'Christmas Day',
+    datetime.date(2010, 5, 1): 'Labour Day',
+    datetime.date(2010, 4, 2): 'Good Friday',
+    datetime.date(2010, 12, 26): 'Second Christmas Day'}
 
 ann_el_demand_per_sector = {
     'g0': 3000,
@@ -15,19 +32,20 @@ ann_el_demand_per_sector = {
     'i0': 3000,
     'g6': 5000}
 
-# create basic dataframe for a given year and include holidays optionally
-dataframe_index = pd.date_range(pd.datetime(year, 1, 1, 0), periods=8760,
-                                freq='H')
-
 # read standard load profiles
-e_slp = bdew.ElecSlp(dataframe_index)
+e_slp = bdew.ElecSlp(year, holidays=holidays)
 
 # multiply given annual demand with timeseries
 elec_demand = e_slp.get_profile(ann_el_demand_per_sector)
 
 # Add the slp for the industrial group
-ilp = profiles.IndustrialLoadProfile(dataframe_index)
+ilp = profiles.IndustrialLoadProfile(e_slp.date_time_index, holidays=holidays)
 elec_demand['i0'] = ilp.simple_profile(ann_el_demand_per_sector['i0'])
+
+# Resample 15-minute values to hourly values.
+elec_demand = elec_demand.resample('H').mean() * 4
+
+print(elec_demand.sum())
 
 # Plot demand
 ax = elec_demand.plot()
