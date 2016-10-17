282dced109df4325f18b6d2e370316a836b37a97
gplessm <guido.plessmann@rl-institut.de>
2016-10-18 01:29:24 +0200

    Move heat example code to function

--- examples/heat_demand_example.py
+++ examples/heat_demand_example.py
@@ -18,33 +18,37 @@ temperature = pd.read_csv(datapath)["temperature"]
 # >>> cal = Germany()
 # >>> holidays = dict(cal.holidays(2010))
 
-holidays = {
-    datetime.date(2010, 5, 24): 'Whit Monday',
-    datetime.date(2010, 4, 5): 'Easter Monday',
-    datetime.date(2010, 5, 13): 'Ascension Thursday',
-    datetime.date(2010, 1, 1): 'New year',
-    datetime.date(2010, 10, 3): 'Day of German Unity',
-    datetime.date(2010, 12, 25): 'Christmas Day',
-    datetime.date(2010, 5, 1): 'Labour Day',
-    datetime.date(2010, 4, 2): 'Good Friday',
-    datetime.date(2010, 12, 26): 'Second Christmas Day'}
 
-# Create DataFrame for 2010
-demand = pd.DataFrame(
-    index=pd.date_range(pd.datetime(2010, 1, 1, 0), periods=8760, freq='H'))
+def heat_example():
+	holidays = {
+	    datetime.date(2010, 5, 24): 'Whit Monday',
+	    datetime.date(2010, 4, 5): 'Easter Monday',
+	    datetime.date(2010, 5, 13): 'Ascension Thursday',
+	    datetime.date(2010, 1, 1): 'New year',
+	    datetime.date(2010, 10, 3): 'Day of German Unity',
+	    datetime.date(2010, 12, 25): 'Christmas Day',
+	    datetime.date(2010, 5, 1): 'Labour Day',
+	    datetime.date(2010, 4, 2): 'Good Friday',
+	    datetime.date(2010, 12, 26): 'Second Christmas Day'}
 
-demand['efh'] = bdew.HeatBuilding(
-    demand.index, holidays=holidays, temperature=temperature, shlp_type='EFH',
-    building_class=1, wind_class=1, annual_heat_demand=25000,
-    name='EFH').get_bdew_profile()
+	# Create DataFrame for 2010
+	demand = pd.DataFrame(
+	    index=pd.date_range(pd.datetime(2010, 1, 1, 0), periods=8760, freq='H'))
 
-demand['mfh'] = bdew.HeatBuilding(
-    demand.index, holidays=holidays, temperature=temperature, shlp_type='MFH',
-    building_class=2, wind_class=0, annual_heat_demand=80000,
-    name='MFH').get_bdew_profile()
+	demand['efh'] = bdew.HeatBuilding(
+	    demand.index, holidays=holidays, temperature=temperature, shlp_type='EFH',
+	    building_class=1, wind_class=1, annual_heat_demand=25000,
+	    name='EFH').get_bdew_profile()
 
-# Plot demand of building
-ax = demand.plot()
-ax.set_xlabel("Date")
-ax.set_ylabel("Heat demand in MW")
-plt.show()
+	demand['mfh'] = bdew.HeatBuilding(
+	    demand.index, holidays=holidays, temperature=temperature, shlp_type='MFH',
+	    building_class=2, wind_class=0, annual_heat_demand=80000,
+	    name='MFH').get_bdew_profile()
+
+	# Plot demand of building
+	ax = demand.plot()
+	ax.set_xlabel("Date")
+	ax.set_ylabel("Heat demand in MW")
+	plt.show()
+if __name__ == '__main__':
+    heat_example()
