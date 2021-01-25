c697a6f31ab139daa10f55ca472533e1ca6f4c7c
Patrik Schönfeldt <patrik@sedna.snft.de>
2021-01-25 09:06:42 +0100

    Split BDEW module

---
+++ demandlib/bdew/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Load profiles
+
+SPDX-FileCopyrightText: Patrik Schönfeldt
+
+SPDX-License-Identifier: MIT
+"""
+
+from .elec_slp import ElecSlp
+from .heat_building import HeatBuilding
