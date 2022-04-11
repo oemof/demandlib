d512612a9cebba6c9a00b27bf22258a3f9b4fa78
uvchik <uvchik.git@posteo.eu>
2022-04-11 11:03:40 +0200

    Use black style

--- src/demandlib/vdi/dwd_try.py
+++ src/demandlib/vdi/dwd_try.py
@@ -1,47 +1,54 @@
 import pandas as pd
 
-def read_DWD_weather_file(weather_file_path):
+
+def read_dwd_weather_file(weather_file_path):
     """Read and interpolate "DWD Testreferenzjahr" files."""
     # The comments in DWD files before the header are not commented out.
     # Thus we have to search for the line with the header information:
     header_row = None
-    with open(weather_file_path, 'r') as rows:
+    with open(weather_file_path, "r") as rows:
         for number, row in enumerate(rows, 1):
             # The header is the row before the appearance of '***'
-            if '***' in row:
+            if "***" in row:
                 header_row = number - 1
                 break
 
     # Plausibility check:
     if header_row is None:
-        logger.error('Error: Header row not found in weather file. '
-                     'Is the data type "DWD" correct? Exiting...')
-        logger.error('File is: ' + weather_file_path)
-        exit()
+        msg = (
+            "Error: Header row not found in weather file. "
+            'Is the data type "DWD" correct? Exiting...\nFile is: '
+            + weather_file_path
+        )
+        raise TypeError(msg)
 
     # Read the file and store it in a DataFrame
     weather_data = pd.read_csv(
         weather_file_path,
         delim_whitespace=True,
-        skiprows=header_row-1,
-        index_col=['MM', 'DD', 'HH'],
-        usecols=['MM', 'DD', 'HH', 'B', 'D', 't', 'WG', 'RF', 'WR', 'N', 'p'],
-        comment='*',
-        )
+        skiprows=header_row - 1,
+        index_col=["MM", "DD", "HH"],
+        usecols=["MM", "DD", "HH", "B", "D", "t", "WG", "RF", "WR", "N", "p"],
+        comment="*",
+    )
 
     # Rename the columns to the TRNSYS standard:
-    weather_data.rename(columns={'B': 'IBEAM_H',
-                                 'D': 'IDIFF_H',
-                                 't': 'TAMB',
-                                 'WG': 'WSPEED',
-                                 'RF': 'RHUM',
-                                 'WR': 'WDIR',
-                                 'N': 'CCOVER',
-                                 'p': 'PAMB'},
-                        inplace=True)
+    weather_data.rename(
+        columns={
+            "B": "IBEAM_H",
+            "D": "IDIFF_H",
+            "t": "TAMB",
+            "WG": "WSPEED",
+            "RF": "RHUM",
+            "WR": "WDIR",
+            "N": "CCOVER",
+            "p": "PAMB",
+        },
+        inplace=True,
+    )
 
     # Add an 'HOUR' column:
-    weather_data['HOUR'] = range(1, 8761)
+    weather_data["HOUR"] = range(1, 8761)
 
     # Make sure all columns are in the correct order
     # weather_data = weather_data.reindex(columns=['HOUR', 'IBEAM_H', 'IDIFF_H',
