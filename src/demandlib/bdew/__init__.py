45818304eecde122e5d75f3a51dadb73435b00f7
Patrik Schönfeldt <patrik@salacia.snft.de>
2025-04-07 20:18:18 +0200

    Add dynamic BDEW25 profiles

--- src/demandlib/bdew/__init__.py
+++ src/demandlib/bdew/__init__.py
@@ -8,7 +8,10 @@ SPDX-License-Identifier: MIT
 """
 
 from ._profiles25 import G25
+from ._profiles25 import H25
 from ._profiles25 import L25
+from ._profiles25 import P25
+from ._profiles25 import S25
 from .elec_slp import ElecSlp
 from .heat_building import HeatBuilding
 
@@ -16,5 +19,8 @@ __all__ = [
     "ElecSlp",
     "HeatBuilding",
     "G25",
+    "H25",
     "L25",
+    "P25",
+    "S25",
 ]
--- src/demandlib/bdew/_profiles25.py
+++ src/demandlib/bdew/_profiles25.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Implementation of the 2025 BDEW standard load profiles:
-* H25 (dynamic), household profile based on 2018 and 2019 data
 * G25 (static), industrial profile based on 2022 and 2023 data
+* H25 (dynamic), household profile based on 2018 and 2019 data
 * L25 (static), farm profile based on previous L0 profile
 * P25 (dynamic), profile for households with PV based on 2022 and 2023 data
 * S25 (dynamic), profile for households with BES based on 2022 and 2023 data
@@ -14,6 +14,7 @@ SPDX-License-Identifier: MIT
 
 import os
 
+import numpy as np
 import pandas as pd
 
 _bdew_datapath = os.path.join(os.path.dirname(__file__), "bdew_data")
@@ -73,7 +74,6 @@ class BDEW25Profile(pd.Series):
         return profile_data
 
     def __init__(self, timeindex: pd.DatetimeIndex):
-
         new_df = pd.DataFrame(
             data={
                 "month": timeindex.month,
@@ -92,9 +92,30 @@ class BDEW25Profile(pd.Series):
             on=["month", "day", "hour", "minute"],
             how="inner",
         )
-        new_df.set_index(dt_index, inplace=True)
+        new_df.set_index(timeindex, inplace=True)
 
         super().__init__(data=new_df.value, index=timeindex)
+
+
+class DynamicBDEW25Profile(BDEW25Profile):
+    def __init__(self, timeindex: pd.DatetimeIndex):
+        super().__init__(timeindex=timeindex)
+        self.update(self * self.dynamisation_function(timeindex))
+
+    @staticmethod
+    def dynamisation_function(timeindex: pd.DatetimeIndex) -> pd.Series:
+
+        # cast to float hear because of miscalculations when using integers
+        day_of_year = np.array(timeindex.day_of_year, dtype=float)
+
+        return pd.Series(
+            -3.92 * 10**-10 * day_of_year**4
+            + 3.2 * 10**-7 * day_of_year**3
+            - 7.02 * 10**-5 * day_of_year**2
+            + 0.0021 * day_of_year
+            + 1.24,
+            index=timeindex,
+        )
 
 
 class G25(BDEW25Profile):
@@ -104,6 +125,15 @@ class G25(BDEW25Profile):
     @property
     def datafile(self):
         return "/g25.csv"
+
+
+class H25(DynamicBDEW25Profile):
+    def __init__(self, timeindex: pd.DatetimeIndex):
+        super().__init__(timeindex=timeindex)
+
+    @property
+    def datafile(self):
+        return "/h25.csv"
 
 
 class L25(BDEW25Profile):
@@ -118,15 +148,19 @@ class L25(BDEW25Profile):
         return "/l25.csv"
 
 
-if __name__ == "__main__":
-    dt_index = pd.date_range(
-        start="2020-01-01 00:00",
-        end="2020-12-31 23:45",
-        freq="15min",
-    )
+class P25(DynamicBDEW25Profile):
+    def __init__(self, timeindex: pd.DatetimeIndex):
+        super().__init__(timeindex=timeindex)
 
-    profile_g = G25(dt_index)
-    profile_l = L25(dt_index)
+    @property
+    def datafile(self):
+        return "/p25.csv"
 
-    print(profile_g)
-    print(profile_l)
+
+class S25(DynamicBDEW25Profile):
+    def __init__(self, timeindex: pd.DatetimeIndex):
+        super().__init__(timeindex=timeindex)
+
+    @property
+    def datafile(self):
+        return "/s25.csv"
