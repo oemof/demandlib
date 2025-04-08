d0737a6acd580c833c7838ac70a8f8af3902e91e
Patrik Schönfeldt <patrik@proxima.snft.de>
2025-04-08 14:30:32 +0200

    Show different resolutions in SLP25 example

--- examples/bdew_slp25.py
+++ examples/bdew_slp25.py
@@ -14,10 +14,20 @@ import pandas as pd
 
 from demandlib import bdew
 
-dt_index = pd.date_range(
+index15m = pd.date_range(
     start="2020-01-01 00:00",
     end="2020-01-31 23:45",
     freq="15min",
+)
+index1m = pd.date_range(
+    start="2020-01-01 00:00",
+    end="2020-01-31 23:45",
+    freq="1min",
+)
+index3h = pd.date_range(
+    start="2020-01-01 00:00",
+    end="2020-01-31 23:45",
+    freq="3h",
 )
 
 holidays = [
@@ -28,14 +38,13 @@ holidays = [
     datetime.date(2020, 1, 5),
 ]
 
-h25_holidays = bdew.H25(dt_index, holidays)
-h25 = bdew.H25(dt_index)
+h25_holidays = bdew.H25(index1m, holidays)
+h25_std = bdew.H25(index15m)
+h25_3h = bdew.H25(index3h)
 
-print(h25.sum() / 4)
-print(h25_holidays.sum() / 4)
-
-plt.plot(h25, label="HWD")
-plt.plot(h25_holidays, label="HHD")
+plt.step(h25_holidays.index, h25_holidays, label="Holidays", where="post")
+plt.step(h25_std.index, h25_std, label="Standard", where="post")
+plt.step(h25_3h.index, h25_3h, label="Three hours", where="post")
 plt.legend()
 
 plt.show()
