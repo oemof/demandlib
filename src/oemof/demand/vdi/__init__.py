1f80a2d7d59f2a8f0a6ca30635c3ac6d56bc8f34
Patrik Schönfeldt <patrik@salacia.snft.de>
2026-02-16 14:07:57 +0100

    Harmonise format of init files

--- src/oemof/demand/vdi/__init__.py
+++ src/oemof/demand/vdi/__init__.py
@@ -1,4 +1,11 @@
-from .dwd_try import find_try_region  # noqa: F401
-from .dwd_try import read_dwd_weather_file  # noqa: F401
-from .regions import Climate  # noqa: F401
-from .regions import Region  # noqa: F401
+from .dwd_try import find_try_region
+from .dwd_try import read_dwd_weather_file
+from .regions import Climate
+from .regions import Region
+
+__all__ = [
+    "find_try_region",
+    "read_dwd_weather_file",
+    "Climate",
+    "Region",
+]
